188d20c81501c21170a7fa284ee73163a4b695ef
SabrinaLeuenberger <sabrina.leuenberger@students.fhnw.ch>
2017-10-01 14:46:37 +0200

    Hangman basic logic

---
+++ src/main/java/hangman/game/HangmanGame.java
@@ -0,0 +1,54 @@
+package hangman.game;
+
+public class HangmanGame {
+
+	private String wordToGuess;
+	private int remainingLives;
+	private char[] displayedWord;
+
+	public HangmanGame(String wordToGuess, int lives) {
+		this.wordToGuess = wordToGuess.toUpperCase();
+		this.remainingLives = lives;
+		displayedWord = new char[wordToGuess.length()];
+		for (int i = 0; i < wordToGuess.length(); i++) {
+			displayedWord[i] = '#';
+		}
+	}
+
+	public int getRemainingLives() {
+		return remainingLives;
+	}
+
+	public String getDisplayedWord() {
+		return String.copyValueOf(displayedWord);
+	}
+
+	public boolean guessCharacter(char c) {
+		char toTest = Character.toUpperCase(c);
+		boolean found = false;
+		for (int i = 0; i < wordToGuess.length(); i++) {
+			if (toTest == wordToGuess.charAt(i)) {
+				displayedWord[i] = toTest;
+				found = true;
+			}
+		}
+		if (!found) {
+			remainingLives--;
+		}
+		return found;
+	}
+
+	public boolean isGameLost() {
+		return remainingLives < 1;
+	}
+
+	public boolean isGameWon() {
+		boolean wonGame = true;
+		for (int i = 0; i < displayedWord.length; i++) {
+			if (displayedWord[i] == '#') {
+				wonGame = false;
+			}
+		}
+		return wonGame;
+	}
+}
---
+++ src/test/java/hangman/game/HangmanGameTest.java
@@ -0,0 +1,60 @@
+package hangman.game;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class HangmanGameTest {
+
+	@Test
+	public void testRemainingLives() {
+		HangmanGame game = new HangmanGame("Hello", 5);
+		assertEquals(5, game.getRemainingLives());
+	}
+
+	@Test
+	public void testDisplayedWord() {
+		HangmanGame game = new HangmanGame("Hello", 1);
+		assertEquals("#####", game.getDisplayedWord());
+	}
+
+	@Test
+	public void testGuessPresentCharacter() {
+		HangmanGame game = new HangmanGame("Hello", 2);
+		assertTrue(game.guessCharacter('E'));
+		assertEquals("#E###", game.getDisplayedWord());
+		assertTrue(game.guessCharacter('l'));
+		assertEquals("#ELL#", game.getDisplayedWord());
+		assertEquals(2, game.getRemainingLives());
+	}
+
+	@Test
+	public void testGuessAbsentCharacter() {
+		HangmanGame game = new HangmanGame("Hello", 2);
+		assertFalse(game.guessCharacter('F'));
+		assertEquals("#####", game.getDisplayedWord());
+		assertEquals(1, game.getRemainingLives());
+	}
+
+	@Test
+	public void testGameLost() {
+		HangmanGame game = new HangmanGame("Hello", 2);
+		assertFalse(game.isGameLost());
+		game.guessCharacter('F');
+		assertFalse(game.isGameLost());
+		game.guessCharacter('R');
+		assertTrue(game.isGameLost());
+	}
+
+	@Test
+	public void testGameWon() {
+		HangmanGame game = new HangmanGame("HE", 2);
+		assertFalse(game.isGameWon());
+		game.guessCharacter('H');
+		assertFalse(game.isGameWon());
+		game.guessCharacter('E');
+		assertTrue(game.isGameWon());
+	}
+}
